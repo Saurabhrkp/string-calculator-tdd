12e70e79611d56f4b16561a82305a2a35fe30a1c
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-07-04 01:57:43 +0530

    feat: passes multiple delimiter cases

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
 export const add = (numbers: string): number => {
 	if (!numbers) return 0
 
-	const delimiterMatch = numbers.match(/^\/\/(.+)\n/)
-	const delimiterRegex = delimiterMatch
-		? new RegExp(delimiterMatch[1], 'g')
-		: /,/
-	console.log(delimiterRegex)
-	const sanitizedNumbers = delimiterMatch
-		? numbers.slice(delimiterMatch[0].length)
-		: numbers
+	let delimiters = [',', '\n']
+	let customDelimiterMatch = numbers.match(/^\/\/(\[.*?\])+\n|^\/\/.\n/)
+	// console.log(customDelimiterMatch)
 
-	const numArray = sanitizedNumbers
-		.split(/[\n]+/)
-		.flatMap((line) => line.split(delimiterRegex))
-		.map(Number)
+	if (customDelimiterMatch) {
+		const delimiterSection = customDelimiterMatch[0]
+		numbers = numbers.substring(delimiterSection.length)
+
+		if (delimiterSection.startsWith('//[')) {
+			// Custom delimiters of any length
+			const matches = delimiterSection.match(/\[(.*?)\]/g)
+			if (matches) {
+				delimiters = matches.map((match) => match.slice(1, -1))
+			}
+		} else {
+			// Single custom delimiter
+			delimiters = [delimiterSection[2]]
+		}
+	}
+
+	// console.log(delimiters)
+	// Create a regex pattern to split by all delimiters
+	let splitPattern = new RegExp(
+		`[${delimiters
+			.map((del) => del.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'))
+			.join('|')}]`
+	)
+
+	let numArray = numbers.split(splitPattern).map((num) => parseInt(num))
 
 	const negatives = numArray.filter((num) => num < 0)
 	if (negatives.length > 0) {

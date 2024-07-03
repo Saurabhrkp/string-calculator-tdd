42514ceb927c42e294059e03da3ba3f7396f9a54
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-07-04 01:10:39 +0530

    test: adds negative number check case

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -29,4 +29,9 @@ describe('add should', () => {
 		expect(add('//;\n1;2')).toBe(3)
 		expect(add('//|\n1|2|3')).toBe(6)
 	})
+
+	it('throw an exception for negative numbers', () => {
+		expect(() => add('1,-2,3')).toThrow('Negative numbers not allowed: -2')
+		expect(() => add('1,-2,-3')).toThrow('Negative numbers not allowed: -2, -3')
+	})
 })

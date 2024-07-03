3695dcd14eb46aec98ca871c8fa46e8ee31c6d9f
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-07-04 01:03:03 +0530

    fix: passes case for new line in input

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-export const add = (input: string): number => {
+export const add = (numbers: string): number => {
+	if (!numbers) return 0
+
+	const numArray = numbers.split(new RegExp(`[,\\n]+`)).map(Number)
+
 	let sum = 0
 
-	for (const number of input.split(',')) {
+	for (const number of numArray) {
 		sum = sum + Number(number)
 	}
 

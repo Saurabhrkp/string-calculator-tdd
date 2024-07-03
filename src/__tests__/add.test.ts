6d9c37648552e6025dbcea4e6c324b1498e7386c
Saurabh Patel <saurabhpatel230@hotmail.com>
2024-07-04 00:59:11 +0530

    test: fails at new line in input

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -7,9 +7,21 @@ describe('add should', () => {
 
 	test(`return number if input is just one number`, () => {
 		expect(add('1')).toEqual(1)
+		expect(add('42')).toBe(42)
 	})
 
 	test(`return sum of two numbers`, () => {
 		expect(add('1,5')).toEqual(6)
+		expect(add('10,20')).toBe(30)
+	})
+
+	it('handle any amount of numbers', () => {
+		expect(add('1,2,3,4,5')).toBe(15)
+		expect(add('10,20,30,40,50')).toBe(150)
+	})
+
+	it('allow new lines between numbers', () => {
+		expect(add('1\n2,3')).toBe(6)
+		expect(add('10\n20\n30')).toBe(60)
 	})
 })
